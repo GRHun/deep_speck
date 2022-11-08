20261bc8ec4ddc06f7131a1fc033200402780ac4
GRHun <GRHunhun@163.com>
2022-11-08 11:04:16 +0800

    finish reading speck

--- ztest/easyfuc_test.py
+++ ztest/easyfuc_test.py
@@ -5,7 +5,7 @@ from speck import *
 
 WORD_SIZE = 16
 
-# 1.urandom
+
 def urandom_test():
     # 返回大小为 size 的字节串，它是适合加密使用的随机字节。
     size = 5 
@@ -13,10 +13,8 @@ def urandom_test():
     print(urandom(n))
     print(urandom(n*2))
     
-
-#  2.make_train_data_test
 def make_train_data_test():
-    n = 5  # 生成10个数据
+    n = 6  # 生成n个数据
     Y = np.frombuffer(urandom(n), dtype=np.uint8)
     print("Y:\t", Y)
     Y_1 = Y & 1
@@ -29,7 +27,7 @@ def make_train_data_test():
     plain0l = np.frombuffer(urandom(2*n),dtype=np.uint16);
     plain0r = np.frombuffer(urandom(2*n),dtype=np.uint16);
     plain1l = plain0l ^ diff[0]; plain1r = plain0r ^ diff[1];
-    print("plain0l\n",plain0l,"\nplain0r\n",plain0r,"\nplain1l\n",plain1l,"\nplain1r\n",plain1r)
+    # print("plain0l\n",plain0l,"\nplain0r\n",plain0r,"\nplain1l\n",plain1l,"\nplain1r\n",plain1r)
 
     num_rand_samples = np.sum(Y==0);
     plain1l[Y==0] = np.frombuffer(urandom(2*num_rand_samples),dtype=np.uint16);
@@ -38,28 +36,31 @@ def make_train_data_test():
     ctdata0l, ctdata0r = encrypt((plain0l, plain0r), ks);
     ctdata1l, ctdata1r = encrypt((plain1l, plain1r), ks);
     # X = convert_to_binary([ctdata0l, ctdata0r, ctdata1l, ctdata1r])
-    print("\nX",[ctdata0l, ctdata0r, ctdata1l, ctdata1r])
+    # print("\nX",[ctdata0l, ctdata0r, ctdata1l, ctdata1r])
 
-
-# 3.pickle
 class Bird(object):
     have_feather = True
     way_of_reproduction  = 'egg'
-
 def test_pickle(Bird):
     summer       = Bird()                 # construct an object
     picklestring = pickle.dumps(summer)   # serialize object
     print(picklestring)
 
-def gen_plain(n):
+def test_gen_small_plain(n):
+  pt0 = np.frombuffer(urandom(n),dtype=np.uint8);
+  pt1 = np.frombuffer(urandom(n),dtype=np.uint8);
+  return(pt0, pt1)
+
+def test_gen_plain(n):
   pt0 = np.frombuffer(urandom(2*n),dtype=np.uint16);
   pt1 = np.frombuffer(urandom(2*n),dtype=np.uint16);
+#   print("pt0:\t",pt0)
+#   print("pt1:\t",pt1)
   return(pt0, pt1);
 
-# *中立比特测试   
 def test_neutal():
     num_struct = 3
-    pt0, pt1 = gen_plain(num_struct)
+    pt0, pt1 = test_gen_plain(num_struct)
     diff=(0x211, 0xa04)
     neutral_bits = [20,21,22,14,15,23]
     p0 = np.copy(pt0)
@@ -75,9 +76,6 @@ def test_neutal():
         p1 = np.concatenate([p1,p1^d1],axis=1)
     p0b = p0 ^ diff[0]; p1b = p1 ^ diff[1]
     return(p0,p1,p0b,p1b)
-
-def test():
-    pass
 
 def hw(v):
   res = np.zeros(v.shape,dtype=np.uint8)
@@ -102,7 +100,7 @@ def test_expand_key(k, t):
     l = list(reversed(k[:len(k)-1]));
     for i in range(t-1):
         l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i);
-    return(ks);
+    
 
     # print("k0\t", k)
     # ks = [0 for i in range(t)]
@@ -116,6 +114,39 @@ def test_expand_key(k, t):
     # for i in range(t-1):
     #     l[i%3], ks[i+1] = enc_one_round((l[i%3], ks[i]), i)
     # return(ks);
+    return(ks);
+
+def test_make_structure(pt0, pt1, diff=(0x211, 0xa04), neutral_bits=[1, 21, 22, 14, 15]):
+    """生成明文结构
+    @para:  (pt0,pt1) - 明文对
+    @example: 
+        pt0, pt1 = gen_plain(n)
+        pt0a, pt1a, pt0b, pt1b = make_structure(pt0, pt1, diff=diff, neutral_bits=neutral_bits)
+    """
+    p0 = np.copy(pt0)
+    p1 = np.copy(pt1)
+    p0 = p0.reshape(-1, 1)
+    p1 = p1.reshape(-1, 1)
+    print("p0\n",p0)
+    print("p1\n",p1)
+    # i 需要区分大于16和小于16的情况
+    for i in neutral_bits:
+        print("\n====>",i)
+        d = 1 << i
+        d0 = d >> 16
+        d1 = d & 0xffff
+        p0 = np.concatenate([p0, p0 ^ d0], axis=1)
+        p1 = np.concatenate([p1, p1 ^ d1], axis=1)
+        print("now p0 is:\n",p0)
+        print("now p1 is:\n",p1)
+    p0b = p0 ^ diff[0]
+    p1b = p1 ^ diff[1]
+    return (p0, p1, p0b, p1b)
+
+
+def test():
+    pass
+
 
 if __name__=="__main__":
     # urandom_test()
@@ -123,7 +154,10 @@ if __name__=="__main__":
     # test_pickle(Bird)
     # test_neutal()
     # test_low_weight()
-
-    test_expand_key((0x1918,0x1110,0x0908,0x0100), 5)
-
+    # test_gen_plain(n=4)
+    # test_expand_key((0x1918,0x1110,0x0908,0x0100), 5)
+    n = 5
+    a, b = test_gen_small_plain(n)
+    test_make_structure(a,b)
+    test()
 
---
+++ ztest/test1.py
@@ -0,0 +1,8 @@
+i = 1
+
+d = 1 << i
+d0 = d >> 16
+d1 = d & 0xffff
+print(d)
+print(d0 )
+print(d1)
