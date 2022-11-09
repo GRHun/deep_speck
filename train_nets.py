a09e533e98ed38083fa6ae91c6864b9fbd6848b7
GRHun <GRHunhun@163.com>
2022-11-09 21:51:56 +0800

    add ppt

--- train_nets.py
+++ train_nets.py
@@ -15,14 +15,18 @@ bs = 5000;
 wdir = './freshly_trained_nets/'
 
 def cyclic_lr(num_epochs, high_lr, low_lr):
-  res = lambda i: low_lr + ((num_epochs-1) - i % num_epochs)/(num_epochs-1) * (high_lr - low_lr);
-  return(res);
+  res = lambda i: low_lr + ((num_epochs-1) - i % num_epochs)/(num_epochs-1) * (high_lr - low_lr)
+  return(res)
 
 def make_checkpoint(datei):
+  """在每个训练期之后保存模型。
+  """
+  # https://keras.io/zh/callbacks/#modelcheckpoint
+  # keras.callbacks.ModelCheckpoint(filepath[保存模型的路径], monitor='val_loss'[被监测的数据], verbose=0, save_best_only=False[True， 被监测数据的最佳模型就不会被覆盖], save_weights_only=False, mode='auto', period=1)
   res = ModelCheckpoint(datei, monitor='val_loss', save_best_only = True);
   return(res);
 
-#make residual tower of convolutional blocks
+#todo make residual tower of convolutional blocks
 def make_resnet(num_blocks=2, num_filters=32, num_outputs=1, d1=64, d2=64, word_size=16, ks=3,depth=5, reg_param=0.0001, final_activation='sigmoid'):
   #Input and preprocessing layers
   inp = Input(shape=(num_blocks * word_size * 2,));
@@ -43,7 +47,14 @@ def make_resnet(num_blocks=2, num_filters=32, num_outputs=1, d1=64, d2=64, word_
     conv2 = BatchNormalization()(conv2);
     conv2 = Activation('relu')(conv2);
     shortcut = Add()([shortcut, conv2]);
-  #add prediction head
+  
+  # add prediction head
+  """The prediction head consists of two hidden layers and one output unit. 
+  The first and second layer are densely connected layers with 64 units. 
+  The first of these layers is followed by a batch normalization layer and a rectifier layer; 
+  the second hidden layer does not use batch normalization but is simply a densely connected layer of 64 relu units. 
+  The final layer consists of a single output unit using a sigmoid activation.
+  """
   flat1 = Flatten()(shortcut);
   dense1 = Dense(d1,kernel_regularizer=l2(reg_param))(flat1);
   dense1 = BatchNormalization()(dense1);
@@ -55,21 +66,31 @@ def make_resnet(num_blocks=2, num_filters=32, num_outputs=1, d1=64, d2=64, word_
   model = Model(inputs=inp, outputs=out);
   return(model);
 
+"""
+wdir = './freshly_trained_nets/'
+"""
 def train_speck_distinguisher(num_epochs, num_rounds=7, depth=1):
-    #create the network
+    #!create the network
     net = make_resnet(depth=depth, reg_param=10**-5);
     net.compile(optimizer='adam',loss='mse',metrics=['acc']);
-    #generate training and validation data
+    
+    #!generate training and validation data
+    # data sets consisting of 10**7 samples were generated for training
+    # The datasets were processed in batches of size 5000. The last 10**6 samples were withheld for validation
     X, Y = sp.make_train_data(10**7,num_rounds);
     X_eval, Y_eval = sp.make_train_data(10**6, num_rounds);
-    #set up model checkpoint
+    
+    #!set up model checkpoint
     check = make_checkpoint(wdir+'best'+str(num_rounds)+'depth'+str(depth)+'.h5');
-    #create learnrate schedule
+    
+    #!create learnrate schedule
     lr = LearningRateScheduler(cyclic_lr(10,0.002, 0.0001));
-    #train and evaluate
+    
+    #!train and evaluate
     h = net.fit(X,Y,epochs=num_epochs,batch_size=bs,validation_data=(X_eval, Y_eval), callbacks=[lr,check]);
     np.save(wdir+'h'+str(num_rounds)+'r_depth'+str(depth)+'.npy', h.history['val_acc']);
     np.save(wdir+'h'+str(num_rounds)+'r_depth'+str(depth)+'.npy', h.history['val_loss']);
+    # 
     dump(h.history,open(wdir+'hist'+str(num_rounds)+'r_depth'+str(depth)+'.p','wb'));
     print("Best validation accuracy: ", np.max(h.history['val_acc']));
     return(net, h);
